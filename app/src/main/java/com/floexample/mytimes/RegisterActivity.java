1152f80b6cddb29fb66c86534ded22228d4bf686
Flo <florin.vaduva@crmsoftware.ro>
2017-11-09 17:13:52 +0200

    testing first commit

--- app/src/main/java/com/floexample/mytimes/RegisterActivity.java
+++ app/src/main/java/com/floexample/mytimes/RegisterActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class RegisterActivity extends AppCompatActivity {
+    //commit test
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
